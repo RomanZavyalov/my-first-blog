1e83a54f01adc5f396fbe211c291d6f96b6d451e
Roman <isip_r.v.zavyalov@mpt.ru>
2021-11-25 19:55:53 +0300

    My app, first commit

---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.index, name='index'),
+        path('crm/', views.crm, name='crm'),
+        path('crm_insert/', views.crm_insert, name='crm_insert'),
+]
---
+++ blog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .models import FB
+import ftplib
+from django.http import HttpResponse
+def index(request):
+    return render(request, 'blog/index.html', {})
+def crm(request):
+    return render(request, 'blog/crm.html', {})
+def crm_insert(request):
+    a = FB(email = request.POST['email_fb'], text = request.POST['text_fb'])
+    a.save()
+    return HttpResponseRedirect(reverse('index'))
